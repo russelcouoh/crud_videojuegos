60fc5f6e55ac606117c21b34195d86ca9b010598
Russel <russeldcouoh@gmail.com>
2024-12-05 01:03:32 -0600

    actualización viewa NADA

--- videojuegos/listavideojuegos/views.py
+++ videojuegos/listavideojuegos/views.py
@@ -2,5 +2,3 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import listavideojuegos
-
-
