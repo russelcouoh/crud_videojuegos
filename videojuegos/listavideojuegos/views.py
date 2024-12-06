de4858df30e3aaa56b6d7e01e29393827c0c8c5d
Russel <russeldcouoh@gmail.com>
2024-12-05 20:22:25 -0600

    agregar borrar a views

--- videojuegos/listavideojuegos/views.py
+++ videojuegos/listavideojuegos/views.py
@@ -2,3 +2,44 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import listavideojuegos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def borrar_videojuego(request, id):
+    videojuego = get_object_or_404(listavideojuegos, id=id)
+    if request.method == 'POST':
+        videojuego.delete()
+        return redirect('inicio')
+    return render(request, 'borrar.html', {'listavideojuegos': videojuego})
+
+   
